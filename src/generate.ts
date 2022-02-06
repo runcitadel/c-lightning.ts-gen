0371e0f4ed3e5339ed40a2e9c06df41e792d2d30
Aaron Dewes <aaron.dewes@protonmail.com>
2022-02-06 11:15:08 +0100

    Mark some constant values as const, fix types

--- src/generate.ts
+++ src/generate.ts
@@ -223,11 +223,11 @@ export default class RPCClient {
   constructor(public socketPath: string) {}
   protected call<ReturnType = unknown>(
     method: string,
-    params = null
+    params: unknown = null
   ): Promise<ReturnType> {
     return new Promise((resolve, reject) => {
-      let client = net.createConnection(this.socketPath);
-      let payload = {
+      const client = net.createConnection(this.socketPath);
+      const payload = {
         method: method,
         params: params,
         id: 0,
@@ -236,7 +236,7 @@ export default class RPCClient {
 
       client.on("data", (data) => {
         client.end();
-        let parsed = JSON.parse(data.toString("utf8"));
+        const parsed = JSON.parse(data.toString("utf8"));
         return resolve(parsed.result as ReturnType);
       });
     });
